472feea1a0fe7adc608bed145f0c23c3bdf19096
osontungo <osontungo@gmail.com>
2024-11-29 15:22:17 +0700

    Update manifest.js

--- config/plugin/manifest.js
+++ config/plugin/manifest.js
@@ -3,8 +3,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `Dimas Prasetyo`,
-        short_name: `prasetyodev`,
+        name: `Sơn Tùng`,
+        short_name: `Đức Minh`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#663399`,
