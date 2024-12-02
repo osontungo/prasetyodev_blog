4a32e912d4b37c9a760d1916c30266ae9d64908c
osontungo <osontungo@gmail.com>
2024-12-02 17:03:28 +0700

    up

--- src/pages/chungchiqlnn.js
+++ src/pages/chungchiqlnn.js
@@ -1,20 +1,83 @@
 import React from "react"
-import Layout from "../components/layout"
+import { Link, graphql } from "gatsby"
 import Seo from "../components/seo"
-import chungchiql from "../components/chungchiql"
+import Layout from "../components/layout"
+import Bio from "../components/bio"
+import BlockElement from "../components/blockElement"
 import PropTypes from "prop-types"
 
-const Personalities = ({ location }) => {
+export const pageQuery = graphql`
+  {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
+      nodes {
+        excerpt
+        fields {
+          slug
+        }
+        frontmatter {
+          date(formatString: "MMMM DD, YYYY")
+          title
+          description
+        }
+      }
+    }
+  }
+`
+
+  const Blog = ({ data, location }) => {
+  const posts = data.allMarkdownRemark.nodes
+
   return (
-    <Layout location={location} title={"Quản Lý Nhà Nước"}>
-      <Seo title="Chứng chỉ" />
-      <MyPersonality />
+    <Layout location={location} title={`Bài viết & Chia sẻ`}>
+      <Seo title="Bài viết & Chia sẻ" />
+      <BlockElement className="blog-wrapper" component="div">
+        <ol style={{ listStyle: `none` }}>
+          {posts.map(post => {
+            const title = post.frontmatter.title || post.fields.slug
+            return (
+              <li key={post.fields.slug}>
+                <article
+                  className="post-list-item"
+                  itemScope
+                  itemType="http://schema.org/Article"
+                >
+                  <header>
+                    <h2>
+                      <Link to={post.fields.slug} itemProp="url">
+                        <span itemProp="headline">{title}</span>
+                      </Link>
+                    </h2>
+                    <small>{post.frontmatter.date}</small>
+                  </header>
+                  <section>
+                    <p
+                      dangerouslySetInnerHTML={{
+                        __html: post.frontmatter.description || post.excerpt,
+                      }}
+                      itemProp="description"
+                    />
+                  </section>
+                </article>
+              </li>
+            )
+          })}
+        </ol>
+        <div className="d-flex justify-content-center p-4">
+          <Bio className="mt-5" />
+        </div>
+      </BlockElement>
     </Layout>
   )
 }
 
-Personalities.propTypes = {
+Blog.propTypes = {
   location: PropTypes.object,
+  data: PropTypes.array
 }
 
-export default Chungchiqlnn
+export default Blog
