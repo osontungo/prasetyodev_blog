69087283f7062ae2708992a4f32ddb1fedfc5d2f
osontungo <osontungo@gmail.com>
2024-12-02 16:38:44 +0700

    Create chungchiql.js

---
+++ src/pages/chungchiql.js
@@ -0,0 +1,20 @@
+import React from "react"
+import Layout from "../components/layout"
+import Seo from "../components/seo"
+import chungchiql from "../components/chungchiql"
+import PropTypes from "prop-types"
+
+const Personalities = ({ location }) => {
+  return (
+    <Layout location={location} title={"Quản Lý Nhà Nước"}>
+      <Seo title="Chứng chỉ" />
+      <MyPersonality />
+    </Layout>
+  )
+}
+
+Personalities.propTypes = {
+  location: PropTypes.object,
+}
+
+export default chungchiql
