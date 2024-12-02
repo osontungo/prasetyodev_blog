92e8dc6974bd29175e88cc209bed47bd57b196aa
osontungo <osontungo@gmail.com>
2024-12-02 22:01:29 +0700

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -18,11 +18,11 @@ const About = ({ location }) => {
           className="col-sm-12 col-md-8 col-lg-10 order-2 order-sm-1 order-md-1 order-lg-1 spacer-0-sm"
         >
           <p className="about-intro">
-            Rất vui được gặp bạn ở đây!  mình là Sơn Tùng, người đam mê khám phá các chủ đề về kinh doanh, phát triển bản thân và đọc sách.
+             Rất vui được gặp bạn ở đây!  mình là Sơn Tùng, người đam mê khám phá các chủ đề về kinh doanh, phát triển bản thân và đọc sách.
              Mình tin rằng mỗi người đều có thể phát triển và vươn tới mục tiêu của mình bằng cách học hỏi và thay đổi tích cực. 
-             Hy vọng có thể chia sẻ những kiến thức và quan điểm này với mọi người, cùng nhau học hỏi và phát triển! <Br></Br>
+             Hy vọng có thể chia sẻ những kiến thức và quan điểm này với mọi người, cùng nhau học hỏi và phát triển! <br />
             Hiện tại mình đang làm việc tại <a href="https://www.thestartup.vn">Công ty Cổ phần tập đoàn The Startup.</a>
-            <br />Mình phụ trách về tuyển dụng Xây dựng phát triển đội ngũ nhân lực kinh doanh, xây dựng kế hoạch, thiết lập 
+            Mình phụ trách về tuyển dụng Xây dựng phát triển đội ngũ nhân lực kinh doanh, xây dựng kế hoạch, thiết lập 
             hệ thống và lãnh đạo quản lý đội nhóm kinh doanh. Rất vui nếu có thể kết nối hỗ trợ mọi người cùng nhau phát triển trong công việc!
           </p>
           <Link href="/personalities">Tìm hiểu thêm</Link>
