8e567e8b58962c54ebeb597fa546a8262d27044b
osontungo <osontungo@gmail.com>
2024-12-02 17:05:38 +0700

    Update chungchiqlnn.js

--- src/pages/chungchiqlnn.js
+++ src/pages/chungchiqlnn.js
@@ -1,83 +1,20 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
-import Seo from "../components/seo"
 import Layout from "../components/layout"
-import Bio from "../components/bio"
-import BlockElement from "../components/blockElement"
+import Seo from "../components/seo"
+import MyPersonality from "../components/myPersonality"
 import PropTypes from "prop-types"
 
-export const pageQuery = graphql`
-  {
-    site {
-      siteMetadata {
-        title
-      }
-    }
-    allMarkdownRemark(sort: { frontmatter: { date: DESC } }) {
-      nodes {
-        excerpt
-        fields {
-          slug
-        }
-        frontmatter {
-          date(formatString: "MMMM DD, YYYY")
-          title
-          description
-        }
-      }
-    }
-  }
-`
-
-  const Blog = ({ data, location }) => {
-  const posts = data.allMarkdownRemark.nodes
-
+const Personalities = ({ location }) => {
   return (
-    <Layout location={location} title={`Bài viết & Chia sẻ`}>
-      <Seo title="Bài viết & Chia sẻ" />
-      <BlockElement className="blog-wrapper" component="div">
-        <ol style={{ listStyle: `none` }}>
-          {posts.map(post => {
-            const title = post.frontmatter.title || post.fields.slug
-            return (
-              <li key={post.fields.slug}>
-                <article
-                  className="post-list-item"
-                  itemScope
-                  itemType="http://schema.org/Article"
-                >
-                  <header>
-                    <h2>
-                      <Link to={post.fields.slug} itemProp="url">
-                        <span itemProp="headline">{title}</span>
-                      </Link>
-                    </h2>
-                    <small>{post.frontmatter.date}</small>
-                  </header>
-                  <section>
-                    <p
-                      dangerouslySetInnerHTML={{
-                        __html: post.frontmatter.description || post.excerpt,
-                      }}
-                      itemProp="description"
-                    />
-                  </section>
-                </article>
-              </li>
-            )
-          })}
-        </ol>
-        <div className="d-flex justify-content-center p-4">
-          <Bio className="mt-5" />
-        </div>
-      </BlockElement>
+    <Layout location={location} title={"Personality"}>
+      <Seo title="Personalities" />
+      <MyPersonality />
     </Layout>
   )
 }
 
-Blog.propTypes = {
+Personalities.propTypes = {
   location: PropTypes.object,
-  data: PropTypes.array
 }
 
-export default Blog
+export default Personalities
