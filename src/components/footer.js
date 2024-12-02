3fa35c1cb6729e0ab5fb07b7bc77b19307b2f3bc
osontungo <osontungo@gmail.com>
2024-12-02 21:31:24 +0700

    Update footer.js

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,7 +3,7 @@ import * as React from "react"
 const Footer = () => {
   return (
     <footer className="footer-area">
-      Copyright <a className="d-block m-3" href="https://desocialworld.com/u/lesontung">Sơn Tùng</a> © {` `} {new Date().getFullYear()}
+      Xây dựng bởi <a className="d-block m-3" href="https://desocialworld.com/u/lesontung">Sơn Tùng</a> © {` `} {new Date().getFullYear()}
     </footer>
   )
 }
