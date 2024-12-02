ab0e130d84cf4feb967e137d457084c649d3c463
osontungo <osontungo@gmail.com>
2024-12-02 21:20:25 +0700

    up

--- src/components/hero.js
+++ src/components/hero.js
@@ -32,11 +32,17 @@ const Hero = () => {
           <p className="lead text-transform-minus-2">
           Tận hưởng <b>từng khoảnh khắc</b>.{" "}
           Nơi ghi lại hành trình tìm kiếm <b>ý nghĩa cuộc sống</b>, qua những điều <b>giản dị</b>{" "} và <b>sâu sắc</b> nhất. 🌟
+          Trong cuộc đời này, không ai bước vào thế giới của ta một cách ngẫu nhiên, và không sự kiện nào xảy ra mà không ẩn chứa 
+          một ý nghĩa sâu xa. Mỗi con người ta gặp, mỗi câu chuyện ta trải qua – dù ngọt ngào hay đắng cay – đều là những mảnh ghép 
+          không thể thiếu, định hình nên con người ta hôm nay. Những cuộc gặp gỡ ấy, những khoảnh khắc ấy, chính là lời nhắc nhở 
+          tinh tế từ cuộc sống rằng mọi thứ đều diễn ra đúng thời điểm, đúng người, và vì một lý do nào đó.<br></br>  🌟Ghi lại, trân trọng 
+          những điều quý giá ấy không chỉ là lưu giữ kỷ niệm, mà còn là biểu hiện của lòng biết ơn – lòng biết ơn với chính 
+          hành trình này, đã mang đến cơ hội để ta học, để ta yêu, và để ta sống một cuộc đời trọn vẹn hơn từng ngày.🌟
           </p>
           <Social />
           <div className="d-grid gap-2 d-md-flex justify-content-md-start">
             <Buttons propClass="btn-bloby" propRedirect="/about">
-              Tìm hiểu thêm
+              Xem thêm
             </Buttons>
           </div>
         </div>
