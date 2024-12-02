d38862dd08f46f8e6aa97276f414c560e3d7cc10
osontungo <osontungo@gmail.com>
2024-12-02 16:21:10 +0700

    Update SkillQLNN.js

--- src/pages/skill/SkillQLNN.js
+++ src/pages/skill/SkillQLNN.js
@@ -6,8 +6,8 @@ import PropTypes from "prop-types"
 
 const Personalities = ({ location }) => {
   return (
-    <Layout location={location} title={"Personality"}>
-      <Seo title="Personalities" />
+    <Layout location={location} title={"Chứng chỉ chuyên viên"}>
+      <Seo title="Chuyên viên quản lý nhà nước" />
       <MyPersonality />
     </Layout>
   )
