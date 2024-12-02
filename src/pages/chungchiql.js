3563dd1fe66895f27d3c24e724b340482180b83e
osontungo <osontungo@gmail.com>
2024-12-02 16:49:25 +0700

    Update chungchiql.js

--- src/pages/chungchiql.js
+++ src/pages/chungchiql.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import chungchiql from "../components/chungchiqlnn"
+import Chungchiqlnn from "../components/chungchiql"
 import PropTypes from "prop-types"
 
 const Personalities = ({ location }) => {
@@ -17,4 +17,4 @@ Personalities.propTypes = {
   location: PropTypes.object,
 }
 
-export default chungchiqlnn
+export default Chungchiqlnn
