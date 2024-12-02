796ce045d209707068e00ded0374e70ade06a461
osontungo <osontungo@gmail.com>
2024-12-02 17:10:43 +0700

    Update chungchiqlnn.js

--- src/pages/chungchiqlnn.js
+++ src/pages/chungchiqlnn.js
@@ -1,14 +1,14 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import MyPersonality from "../components/myPersonality"
+import Qlnn from "../components/qlnn"
 import PropTypes from "prop-types"
 
-const Personalities = ({ location }) => {
+const Qlnn = ({ location }) => {
   return (
     <Layout location={location} title={"Personality"}>
       <Seo title="Personalities" />
-      <MyPersonality />
+      <Qlnn />
     </Layout>
   )
 }
