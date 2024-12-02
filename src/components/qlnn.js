07638a33b0715840f286b9ac0e01ce5452ca589b
osontungo <osontungo@gmail.com>
2024-12-02 17:25:39 +0700

    Update qlnn.js

--- src/components/qlnn.js
+++ src/components/qlnn.js
@@ -26,7 +26,11 @@ const qlnn = () => {
     <BlockElement className="row mb-5 mt-5" component="div">
       <div className="col-sm-12 col-md-6 col-lg-6">
         <p className="mt-5">
-          <b>Chứng chỉ Chuyên Viên Quản lý Nhà Nước</b> là abcxyz....
+          <b>Chứng chỉ Chuyên Viên Quản lý Nhà Nước</b> văn bằng xác nhận đã hoàn thành khóa 
+          đào tạo về kiến thức và kỹ năng quản lý hành chính, bao gồm các lĩnh vực như pháp luật,
+           chính sách công, tổ chức bộ máy nhà nước, công tác văn thư, quản lý nhân sự, và kỹ năng
+            xử lý công việc hành chính. Chứng chỉ này thường cần thiết để bổ nhiệm, nâng ngạch, 
+            hoặc đáp ứng tiêu chuẩn công việc trong các cơ quan nhà nước.
         </p>
       </div>
       <div className="col-sm-12 col-md-6 col-lg-6">
