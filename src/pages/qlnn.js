38aff2ad230e971aa2e88f0e10e3bb5ba0a586d2
osontungo <osontungo@gmail.com>
2024-12-02 16:30:53 +0700

    Update qlnn.js

--- src/pages/qlnn.js
+++ src/pages/qlnn.js
@@ -1,7 +1,7 @@
 import React from "react"
 import Layout from "../components/layout"
 import Seo from "../components/seo"
-import MyPersonality from "../components/CCQLNN"
+import CCQLNN from "../components/CCQLNN"
 import PropTypes from "prop-types"
 
 const CCQLNN = ({ location }) => {
